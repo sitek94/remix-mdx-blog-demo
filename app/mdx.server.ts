69634d9fd11aaefb4653d34c714d1d7a2ce2f1e7
Maciek <msitkowski94@gmail.com>
2022-07-30 15:25:18 +0200

    use remark-mdx-images to bundle local images + workaround for ESM in Remix

--- app/mdx.server.ts
+++ app/mdx.server.ts
@@ -6,6 +6,23 @@ import type { MdxListItem, MdxPage } from '~/types'
 
 const blogPostsPath = path.join(process.cwd(), 'content/blog')
 
+if (process.platform === 'win32') {
+  process.env.ESBUILD_BINARY_PATH = path.join(
+    process.cwd(),
+    'node_modules',
+    'esbuild',
+    'esbuild.exe',
+  )
+} else {
+  process.env.ESBUILD_BINARY_PATH = path.join(
+    process.cwd(),
+    'node_modules',
+    'esbuild',
+    'bin',
+    'esbuild',
+  )
+}
+
 export async function getBlogMdxPages(): Promise<MdxPage[]> {
   const paths = sync(`${blogPostsPath}/**/*.mdx`)
 
@@ -38,8 +55,35 @@ export async function getBlogMdxListItems(): Promise<MdxListItem[]> {
 }
 
 export async function getBlogMdxPage(slug: string) {
+  const { default: remarkMdxImages } = await import('remark-mdx-images')
+
   const page = await bundleMDX({
     file: `${blogPostsPath}/${slug}.mdx`,
+    cwd: process.cwd(),
+    mdxOptions: options => {
+      options.remarkPlugins = [
+        ...(options.remarkPlugins ?? []),
+        remarkMdxImages,
+      ]
+
+      return options
+    },
+    esbuildOptions: options => {
+      // Set the `outdir` to a public location for this bundle.
+      options.outdir = `${process.cwd()}/public/images/blog`
+      options.loader = {
+        ...options.loader,
+        // Tell esbuild to use the `file` loader for pngs
+        '.png': 'file',
+      }
+      // Set the public path to /img/about
+      options.publicPath = '/images/blog'
+
+      // Set write to true so that esbuild will output the files.
+      options.write = true
+
+      return options
+    },
   })
 
   return page
