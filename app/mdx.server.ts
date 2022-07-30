0a5d36430a73c5947a3f4a4093e99ee65472252b
Maciek <msitkowski94@gmail.com>
2022-07-30 16:34:16 +0200

    use directories for posts with local files

--- app/mdx.server.ts
+++ app/mdx.server.ts
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import path from 'path'
 import { sync } from 'glob'
 import { bundleMDX } from 'mdx-bundler'
@@ -6,6 +7,8 @@ import type { MdxListItem, MdxPage } from '~/types'
 
 const blogPostsPath = path.join(process.cwd(), 'content/blog')
 
+// Esbuild sometimes can't figure out where to look for its executables
+// https://github.com/kentcdodds/mdx-bundler#nextjs-esbuild-enoent
 if (process.platform === 'win32') {
   process.env.ESBUILD_BINARY_PATH = path.join(
     process.cwd(),
@@ -28,13 +31,8 @@ export async function getBlogMdxPages(): Promise<MdxPage[]> {
 
   const pages = await Promise.all(
     paths.map(async path => {
-      const slug = path
-        .replace(blogPostsPath, '')
-        .replace('.mdx', '')
-        .replace('/', '')
-
       return {
-        slug,
+        slug: slugifyBlogPostPath(path),
         ...(await bundleMDX({
           file: path,
           // cwd: '/Users/sitek/sandbox/remix-mdx-blog-demo',
@@ -57,8 +55,15 @@ export async function getBlogMdxListItems(): Promise<MdxListItem[]> {
 export async function getBlogMdxPage(slug: string) {
   const { default: remarkMdxImages } = await import('remark-mdx-images')
 
+  const fileOrDirectoryPath = path.join(blogPostsPath, slug)
+
+  // If the path is a directory, we'll try to find the index.mdx file, otherwise we'll just use the path
+  const filePath = isDirectory(fileOrDirectoryPath)
+    ? path.join(fileOrDirectoryPath, 'index.mdx')
+    : `${fileOrDirectoryPath}.mdx`
+
   const page = await bundleMDX({
-    file: `${blogPostsPath}/${slug}.mdx`,
+    file: filePath,
     cwd: process.cwd(),
     mdxOptions: options => {
       options.remarkPlugins = [
@@ -87,4 +92,29 @@ export async function getBlogMdxPage(slug: string) {
   })
 
   return page
+}
+
+/**
+ * Check if given path is a directory
+ */
+function isDirectory(path: string) {
+  try {
+    return fs.lstatSync(path).isDirectory()
+  } catch {
+    return false
+  }
+}
+
+function slugifyBlogPostPath(path: string) {
+  return (
+    path
+      // Remove base blog post path
+      .replace(blogPostsPath, '')
+      // Remove file extension
+      .replace('.mdx', '')
+      // Remove leading slash
+      .replace('/', '')
+      // Remove index (for files in directories)
+      .replace('/index', '')
+  )
 }
